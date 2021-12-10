03afc2812a5feb0e6d066cc5070752ae155aa746
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-12-10 13:47:20 -0300

    Invite Modal created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,169 @@
-import Header from '../../components/Header'
 import BackImg from '../../assets/bemol-shop.jpg'
 import Container from './styles'
+import Header from '../../components/Header'
+import { useState } from 'react'
+import Button from '../../components/Button'
 
 const Home = () => {
+    const [values, setValues] = useState("");
+    const [tags, setTags] = useState<string[]>([]);
+
+    const handlePaste = (e: any, ecurr: any) => {
+        let paste;
+        let newArr : string[] = [];
+        let tag;
+
+        e.preventDefault(); 
+        
+        paste = ecurr.replaceAll(" ", "").split(',')
+
+        for(let i = 0; i < tags.length; i++){
+            tag = tags[i]
+            
+            newArr.push(tag)
+        }
+        
+        for (let i = 0; i < paste.length; i++){
+            tag = paste[i]
+            
+            newArr.push(tag)
+        }
+        
+        setTags(newArr)
+
+        setValues("")
+    }
+
+    const handleRemoveTag = (tag: string) => {
+        const newTags = tags.filter(t => t !== tag)
+        setTags(newTags);
+    }
+    
+    const handleValidate = (tag: string) => {
+        let hashReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let result = false
+        
+        if(hashReg.test(tag)){
+            result = true
+        }
+        return result
+    }
+
+    const handleUpdateTags = (e: any) => {
+        e.preventDefault();  
+
+        if(values.length > 0){
+            if(e.key === ',' || e.key === 'Enter' || e.key === ' '){
+                if(e.key === ','){
+                    const newTag = values.slice(0,-1);
+                    const newArr = [...tags, newTag]
+                    setTags(newArr)
+                } else if(e.key === 'Enter' || e.key === ' '){
+                    const newTag = values;
+                    const newArr = [...tags, newTag]
+                    setTags(newArr)
+                }
+                setValues("")
+            }
+        }
+
+        if(values === '' && e.key === 'Backspace'){
+            const otherTags = [...tags];
+            otherTags.pop();
+            setTags(otherTags);
+        }
+
+    }
+
+    const isAllValid = (tags: string[]) => {
+        let result = 0
+        let tag;
+
+        for (let i = 0; i < tags.length; i++ ){
+            tag = tags[i]
+
+            if(handleValidate(tag) === false){
+                result ++
+            }
+        }
+        
+        return result
+    }
+
+    const deleteAllWrongTags = (tags: string[]) => { 
+        let newArr: string[] = []
+        let tag: string;
+
+        for(let i = 0; i < tags.length; i++){
+            tag = tags[i]
+
+            if(handleValidate(tag) == true){
+                newArr.push(tag)
+            }
+        }
+        setTags(newArr);
+    }
+
+    const handleRenderErrorMsg = () => {
+        return (
+            <div className="errorText">
+                <span onClick={() => deleteAllWrongTags(tags)} style={{ color: '#E6535F' }} className={"removeTags"}>
+                    Remover todos os emails com erro
+                </span>
+            </div>
+             
+        )     
+    }
+
     return (
         <div>
             <Header />
             <Container>
                 <div>
                     <img src={BackImg} alt="" />
+                </div>
+                <div className='shareModal'>
+                    <h2>Convide seus amigos</h2>
+
+                    <span>Insira os emails no campo abaixo</span>
+                    
+                    <div className='inputContainer'>
+                        {tags.map((tag, i) => (
+                            handleValidate(tag) === true ? (
+                                <div className="validTagContainer" key={i}>
+                                    <p>{tag} <span onClick={() => handleRemoveTag(tag)}> x </span></p>
+                                </div>
+                            ) : (
+                                <div className="invalidTagContainer" key={i}>
+                                    <p>                                           
+                                        {tag} <span onClick={() => handleRemoveTag(tag)}> x </span>
+                                    </p>
+                                </div>
+                            )
+                        ))}
+                            
+                        <div>
+                            <input 
+                                type="text"
+                                onPaste={(e) => handlePaste(e, e.clipboardData.getData("Text"))}
+                                value={values}
+                                onChange={(e) => setValues(e.target.value)}
+                                onKeyUp={handleUpdateTags}
+                                onKeyDown={(e) => e.key === 'Enter' || e.key === ',' || e.key === ' ' && e.preventDefault()}
+                                onKeyPress={(e) => e.key === 'Enter' || e.key === ',' || e.key === ' ' && e.preventDefault()}
+                            />
+                        </div>
+                    </div>
+
+                    <div className={"error-container"}>
+                        {isAllValid(tags) > 0 && handleRenderErrorMsg()}
+                    </div>
+
+                    <Button
+                        setColor='#0192d5' 
+                        setFontColor='#ffffff'
+                        setSize='giant'
+                    >Enviar</Button>
                 </div>
             </Container>
         </div>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,6 +10,7 @@ const Container = styled.div`
     text-align: center;
     height: 100vh;
     width: 100vw;
+
     img{
         position: absolute;
         left: 0px;
@@ -17,9 +18,128 @@ const Container = styled.div`
         z-index: -1;
         opacity: 0.4;
         width: 100vw;
-        height: 80%;
+        height: 90%;
     }
-    .
+
+    .shareModal{
+        margin-top: 10rem;
+        width: 600px;
+        height: 350px;
+        border-radius: 15px;
+        background: #ffffff;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .inputContainer{
+            border-radius: 4px;
+            border: solid 1px #c4c4c4;
+            padding: 8px 18px;
+            padding-top: 0.8rem;
+            width: 80%;
+            max-heigth: 35%
+            margin: 12px 0 8px 0;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            overflow: scroll;
+            overflow-y: auto;
+            scroll-behavior: auto;
+
+            input{
+                border: none;
+            }
+
+            input:focus {
+                box-shadow: 0 0 0 0;
+                outline: 0;
+            }
+
+            &::-webkit-scrollbar {
+                width: 3px;
+                height: 3px;
+            }
+        
+            &::-webkit-scrollbar-thumb {
+                background-color: #6E7475;
+                border-radius: 20px;
+            }
+        
+            scrollbar-width: thin;
+            scrollbar-color: #f8f9fa #6E7475;
+        }
+
+        .validTagContainer{
+            background: #9BDEFF;
+            color: #1D1E1F;
+            border-radius: 16px;
+            padding-left: 0.5rem; 
+            padding-right: 0.5rem;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+            margin-right: 0.5rem;
+            margin-bottom: 0.55rem;
+            width: auto;
+            min-width: 0.5rem;
+            max-height: 1.7rem;
+            &:hover {
+                background-color: #9BDEFE;
+                opacity: 0.85;
+            }
+        
+            span{
+                margin-left: 0.15rem;
+                color: #3A71AB;
+                cursor: pointer;
+            }
+        }
+
+        .invalidTagContainer {
+            background: #FFD4D8;
+            color: #1D1E1F;
+            border-radius: 16px;
+            padding-left: 0.5rem; 
+            padding-right: 0.5rem;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-content: center;
+            align-items: center;
+            margin-right: 0.5rem;
+            margin-bottom: 0.55rem;
+            width: auto;
+            min-width: 0.5rem;;
+            max-height: 1.7rem;
+            &:hover {
+                background-color: #FFD4D7;
+                opacity: 0.85;
+            }
+        
+            span{
+                margin-left: 0.15rem;
+                color: #E6535F;
+                cursor: pointer;
+            }       
+        }
+
+        .errorText{
+            margin-bottom: 0px;
+
+            .removeTags{
+                cursor: pointer;
+            }
+        }
+
+        .icon {
+            cursor: pointer;
+            margin-left: auto;
+            margin-right: 5px;
+        }
+    }
 `
 
 export default Container
